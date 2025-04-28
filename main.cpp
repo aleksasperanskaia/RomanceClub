2cacfd94018b6d9b56333cefbc63b4e59e95d238
aleksasperanskaia <aleksandrasperanskaya@gmail.com>
2025-04-28 23:26:18 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
-#include <QApplication>
 #include "romanceclub.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    // Инициализация приложения
     QApplication a(argc, argv);
-
-    // Установка стиля для более приятного отображения
-    a.setStyle("Fusion");
-
-    // Создание главного окна приложения
-    RomanceClub window;
-
-    // Настройка размера окна
-    window.resize(800, 600);
-    window.setMinimumSize(640, 480);
-
-    // Загрузка истории и запуск первой сцены
-    window.loadStory();
-    window.start();
-
-    // Отображение окна
-    window.show();
-
-    // Запуск основного цикла приложения
+    RomanceClub w;
+    w.show();
     return a.exec();
 }
